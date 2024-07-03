1a85c4751c13f5873043c4ab1f1b099f6c57d5c1
Evil2997 <dima.molchanow2012@gmail.com>
2024-07-03 13:18:45 +0300

    prints and logs.txt saved in variable

--- core/producer.py
+++ core/producer.py
@@ -14,6 +14,7 @@ class Producer:
         self.__source_url_obj = lib.urlentity.URLEntity(source_url)
         self.waiting_list = [self.__source_url_obj.get_url()]
         self.crawled_list = [self.__source_url_obj.get_url()]
+        self.results = []
 
         # Create the directory for log files such as the list of URLs
         log_dir = os.path.join(lib.common.CONFIG['project_path'], 'log', self.__source_url_obj.get_hostname())
@@ -38,7 +39,6 @@ class Producer:
 
     def run(self):
         lib.colorprint.color().blue('[*] Scanning')
-        success_targets = []
         while not lib.common.FLAG['producer_done']:
             if lib.common.FLAG['stop_signal']:
                 break
@@ -47,8 +47,9 @@ class Producer:
                 if lib.common.FLAG['stop_signal']:
                     break
 
-                print('\r+ ' + self.waiting_list[0])
-                url_obj_to_be_checked = lib.urlentity.URLEntity(self.waiting_list[0])
+                current_url = self.waiting_list[0]
+                print('\r+ ' + current_url)
+                url_obj_to_be_checked = lib.urlentity.URLEntity(current_url)
 
                 # Skip certain file types
                 while url_obj_to_be_checked.get_file().lower().endswith(('png', 'jpg', 'bmp', 'gif')):
@@ -58,16 +59,19 @@ class Producer:
                         break
 
                 try:
-                    response = requests.get(url=self.waiting_list[0])
+                    response = requests.get(url=current_url)
                     soup = bs4.BeautifulSoup(response.text, 'html.parser')
-                    success_targets.append(self.waiting_list[0])
+
+                    # Save the current URL to results as a dictionary
+                    self.results.append({"URL": current_url})
+
                 except Exception as e:
-                    print(f"Request failed for {self.waiting_list[0]}: {e}")
+                    print(f"Request failed for {current_url}: {e}")
                     self.waiting_list.pop(0)
                     continue
 
-                lib.common.CHECKER_OBJ.queue_add(url=self.waiting_list[0])
-                self.log_fp.write(self.waiting_list[0] + '\n')
+                lib.common.CHECKER_OBJ.queue_add(url=current_url)
+                self.log_fp.write(current_url + '\n')
                 self.waiting_list.pop(0)
 
                 # Find new URLs from the current page
@@ -82,4 +86,7 @@ class Producer:
 
 
 if __name__ == '__main__':
-    Producer('http://testphp.vulnweb.com/').run()
+    producer = Producer('http://testphp.vulnweb.com/')
+    producer.run()
+    # Print results
+    print(producer.results)
