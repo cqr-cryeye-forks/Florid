fce5878490d1bcfbf48195b9049c9fd233f89022
Evil2997 <dima.molchanow2012@gmail.com>
2024-07-03 12:53:21 +0300

    Fixed a bug with a declared variable

--- module/phase_two/pathleak.py
+++ module/phase_two/pathleak.py
@@ -12,21 +12,24 @@ def init():
 def run(url):
     url_obj = lib.urlentity.URLEntity(raw_url=url)
 
+    # Check if the URL points to a file
     if url_obj.is_file():
-        if url_obj.get_query() != '':
-            query_list = url_obj.get_query().split('&')
+        query = url_obj.get_query()
+        if query:
+            query_list = query.split('&')
             for item_num in range(len(query_list)):
-                tmp_query_list = list(query_list)
-                tmp_query_list[item_num].replace(tmp_query_list[item_num].split('=')[-1], '')
-                tmp_url = url_obj.get_source() + url_obj.get_file() + '?' + ''.join(tmp_query_list)
+                tmp_query_list = query_list.copy()
+                # Remove the value for the current query parameter
+                tmp_query_list[item_num] = tmp_query_list[item_num].split('=')[0] + '='
+                tmp_url = f"{url_obj.get_source()}{url_obj.get_file()}?{'&'.join(tmp_query_list)}"
                 tmp_url_obj = lib.urlentity.URLEntity(raw_url=tmp_url)
                 tmp_url_obj.make_get_request()
                 html_text = tmp_url_obj.get_response().text.lower()
-                if 'directory listing' in html_text.lower():
+                if 'directory listing' in html_text:
                     lib.common.RESULT_DICT[MODULE_NAME].append(tmp_url)
 
-    if not url_obj.is_file():
-        if 'directory listing' in url_obj.get_response().text.lower():
-            lib.common.RESULT_DICT[MODULE_NAME].append(tmp_url)
+    # Check if the URL points to a directory and contains 'directory listing' in the response
+    elif 'directory listing' in url_obj.get_response().text.lower():
+        lib.common.RESULT_DICT[MODULE_NAME].append(url)
 
     lib.common.ALIVE_LINE[MODULE_NAME] += 1
