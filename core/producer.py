798e4170fce5210f36afba6f315e06650ef920a7
Evil2997 <dima.molchanow2012@gmail.com>
2024-07-04 17:54:10 +0300

    Add exception if IndexError

--- core/producer.py
+++ core/producer.py
@@ -1,13 +1,13 @@
 from __future__ import absolute_import, print_function
 
 import os
+import logging
 from urllib.parse import urljoin
 import bs4
 import requests
 import lib.colorprint
 import lib.common
 import lib.urlentity
-
 
 class Producer:
     def __init__(self, source_url):
@@ -16,15 +16,16 @@ class Producer:
         self.crawled_list = [self.__source_url_obj.get_url()]
         self.results = []
 
-        # Create the directory for log files such as the list of URLs
+        # Set up logging
         log_dir = os.path.join(lib.common.CONFIG['project_path'], 'log', self.__source_url_obj.get_hostname())
         if not os.path.exists(log_dir):
             try:
                 os.makedirs(log_dir)
             except Exception as e:
-                print('Creating Log File Failed:', e)
+                logging.error('Creating Log File Failed: %s', e)
 
         self.log_fp = open(os.path.join(log_dir, 'urllist.txt'), 'w+')
+        logging.basicConfig(level=logging.INFO)
 
     def __find_joint(self, soup, tags, attribute):
         """Finds new URLs from the given soup object and adds them to the waiting and crawled lists."""
@@ -48,13 +49,17 @@ class Producer:
                     break
 
                 current_url = self.waiting_list[0]
-                print('\r+ ' + current_url)
+                logging.info('+ %s', current_url)
                 url_obj_to_be_checked = lib.urlentity.URLEntity(current_url)
 
                 # Skip certain file types
                 while url_obj_to_be_checked.get_file().lower().endswith(('png', 'jpg', 'bmp', 'gif')):
                     if self.waiting_list:
                         self.waiting_list.pop(0)
+                        if not self.waiting_list:
+                            break
+                        current_url = self.waiting_list[0]
+                        url_obj_to_be_checked = lib.urlentity.URLEntity(current_url)
                     else:
                         break
 
@@ -66,13 +71,15 @@ class Producer:
                     self.results.append({"URL": current_url})
 
                 except Exception as e:
-                    print(f"Request failed for {current_url}: {e}")
-                    self.waiting_list.pop(0)
+                    logging.error("Request failed for %s: %s", current_url, e)
+                    if self.waiting_list:
+                        self.waiting_list.pop(0)
                     continue
 
                 lib.common.CHECKER_OBJ.queue_add(url=current_url)
                 self.log_fp.write(current_url + '\n')
-                self.waiting_list.pop(0)
+                if self.waiting_list:
+                    self.waiting_list.pop(0)
 
                 # Find new URLs from the current page
                 self.__find_joint(soup, tags='a', attribute='href')
